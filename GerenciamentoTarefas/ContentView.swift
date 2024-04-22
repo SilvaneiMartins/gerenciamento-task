feb63eb6f3618323f8fe874d6b649fbc70d7f6b8
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2024-04-22 11:25:22 -0400

    Initial Commit

---
+++ GerenciamentoTarefas/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GerenciamentoTarefas
+//
+//  Created by Silvanei  Martins on 22/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GerenciamentoTarefas/GerenciamentoTarefasApp.swift
@@ -0,0 +1,17 @@
+//
+//  GerenciamentoTarefasApp.swift
+//  GerenciamentoTarefas
+//
+//  Created by Silvanei  Martins on 22/04/24.
+//
+
+import SwiftUI
+
+@main
+struct GerenciamentoTarefasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
